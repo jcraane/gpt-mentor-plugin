ce8215603ae9a23330e910b1aa4f4e6101d3145b
GitHub Action <action@github.com>
2023-01-11 20:32:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/jcraane/gptmentorplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jcraane/gptmentorplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jcraane.gptmentorplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jcraane.gptmentorplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/jcraane/gptmentorplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jcraane/gptmentorplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jcraane.gptmentorplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jcraane.gptmentorplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
