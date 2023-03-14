2d9afe3d52dd30d172846b9cb4bc9e9696ed36ec
jamiecraane <jcraane@anwb.nl>
2023-03-14 22:21:44 +0100

    Add prompts for all other actions except improve code

--- src/main/kotlin/com/github/jcraane/gptmentorplugin/openapi/OpenApi.kt
+++ src/main/kotlin/com/github/jcraane/gptmentorplugin/openapi/OpenApi.kt
@@ -29,11 +29,19 @@ sealed class BasicPrompt(
         }
     }
 
-    data class ExplainCode(val code: String) : BasicPrompt("Explain code: \n\n$code", "You are a senior AI programmer.")
-    data class ImproveCode(val code: String) : BasicPrompt("Improve this code: \n\n$code", "You are a senior AI programmer.")
+    data class ExplainCode(val code: String) : BasicPrompt(
+        "Explain code: \n\n$code", """
+        You are an expert AI programmer and an expert in explaining code to medior and junior programmers.
+
+        - Explain the code in concise sentences
+        - Provide examples when possible to explain what the code does
+    """.trimIndent()
+    )
+
+    data class ImproveCode(val code: String) : BasicPrompt("Improve this code: \n\n$code", "You are an expert AI programmer.")
     data class ReviewCode(val code: String) : BasicPrompt(
         "Review this code: \n\n$code", """
-        You are a senior AI programmer reviewing code written by others. During the review:
+        You are an expert AI programmer reviewing code written by others. During the review:
 
         1. Summarize what is good about the code
         2. Mention where the code can be improved and why if applicable
@@ -43,8 +51,24 @@ sealed class BasicPrompt(
     """.trimIndent()
     )
 
-    data class CreateUnitTest(val code: String) : BasicPrompt("Create a unit test for : \n\n$code", "You are a senior AI programmer.")
-    data class AddComments(val code: String) : BasicPrompt("Add docs to this code: \n\n$code", "You are a senior AI programmer.")
+    data class CreateUnitTest(val code: String) : BasicPrompt("Create a unit test for : \n\n$code", """
+        You are an expert AI programmer which uses unit tests to verify behavior.
+
+        - Write unit tests for the obvious cases
+        - Write unit tests for the edge cases
+        - DO NOT EXPLAIN THE TEST
+    """.trimIndent())
+    data class AddComments(val code: String) : BasicPrompt(
+        "Add docs to this code: \n\n$code", """
+        You are an expert AI programmer which writes code documentation to explain code to other developers.
+
+        - Add documentation for explaining non-obvious things
+        - Do not write comments which are obvious in the code
+        - ONLY WRITE DOCS
+        - DO NOT PROVIDE AN EXPLANATION OF THE CODE
+    """.trimIndent()
+    )
+
     data class Chat(val messages: List<ChatGptRequest.Message>) :
         BasicPrompt(messages.lastOrNull()?.content ?: "", "You are a senior AI programmer.") {
         override fun createRequest(): ChatGptRequest {
@@ -60,4 +84,3 @@ sealed class BasicPrompt(
         }
     }
 }
-
