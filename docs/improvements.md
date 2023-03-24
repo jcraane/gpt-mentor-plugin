f19b9c9079e23c72375e3c0278af9afbbf5deab3
jamiecraane <jcraane@anwb.nl>
2023-03-24 12:16:29 +0100

    Add send to gpt mentor action to separator group since more actions will be part if it

--- docs/improvements.md
+++ docs/improvements.md
@@ -4,8 +4,7 @@
 - Add option to create new file from selection
 - Integrate diff action for selected code
 - Add option to move git commits to chat prompt view
-- right-click actions
-- right-click on file and sent to gpt-mentor promp
+- Also append selected text from the editor to the prompt
 
 - Add code formatting if code is detected. When proper chatview with different panels it is perhaps easier to detekt.
 - Add option to remove chat messages (single thread) (from bottom to top)
--- src/main/kotlin/dev/jamiecraane/gptmentorplugin/actions/PromptFromFileAction.kt
+++ src/main/kotlin/dev/jamiecraane/gptmentorplugin/actions/PromptFromFileAction.kt
@@ -2,9 +2,25 @@ package dev.jamiecraane.gptmentorplugin.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import dev.jamiecraane.gptmentorplugin.configuration.GptMentorSettingsState
+import dev.jamiecraane.gptmentorplugin.domain.PromptFactory
+import dev.jamiecraane.gptmentorplugin.messagebus.CHAT_GPT_ACTION_TOPIC
 
 class PromptFromFileAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        println("Executed PromptFromFileAction")
+        val project = e.getData(CommonDataKeys.PROJECT)
+        val psiFile = e.getData(CommonDataKeys.PSI_FILE)
+
+        if (project == null || psiFile == null) {
+            return
+        }
+
+        val virtualFile = psiFile.virtualFile
+        val contents = String(virtualFile.contentsToByteArray())
+        val promptFactory = PromptFactory(project.getService(GptMentorSettingsState::class.java))
+        promptFactory.promptFromSelection(contents).let { prompt ->
+            project.messageBus.syncPublisher(CHAT_GPT_ACTION_TOPIC).onNewPrompt(prompt)
+        }
     }
 }
