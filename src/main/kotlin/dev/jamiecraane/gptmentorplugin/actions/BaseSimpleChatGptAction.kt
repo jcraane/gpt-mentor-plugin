9fd240ca4b2862cf8889ec93bba44be40fd19733
jamiecraane <jcraane@anwb.nl>
2023-03-28 22:06:54 +0200

    Fix issue https://github.com/jcraane/gpt-mentor-plugin/issues/1

--- src/main/kotlin/dev/jamiecraane/gptmentorplugin/actions/BaseSimpleChatGptAction.kt
+++ src/main/kotlin/dev/jamiecraane/gptmentorplugin/actions/BaseSimpleChatGptAction.kt
@@ -8,6 +8,7 @@ import dev.jamiecraane.gptmentorplugin.messagebus.CHAT_GPT_ACTION_TOPIC
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.wm.ToolWindowManager
 import dev.jamiecraane.gptmentorplugin.ui.GptMentorToolWindowFactory
 
@@ -22,15 +23,17 @@ abstract class BaseSimpleChatGptAction : AnAction() {
             return
         }
 
+        ToolWindowManager.getInstance(project).getToolWindow(GptMentorToolWindowFactory.ID)?.show()
+
         if (!this::promptFactory.isInitialized) {
-            promptFactory = PromptFactory(project.getService(GptMentorSettingsState::class.java))
+            val state = ApplicationManager.getApplication().getService(GptMentorSettingsState::class.java)
+            promptFactory = PromptFactory(state)
         }
 
         val selectedText = editor.selectionModel.selectedText
         selectedText?.let { code ->
             val prompt = createPrompt(code)
             project.messageBus.syncPublisher(CHAT_GPT_ACTION_TOPIC).onNewPrompt(prompt)
-            ToolWindowManager.getInstance(project).getToolWindow(GptMentorToolWindowFactory.ID)?.show()
         }
     }
 
